588e9f5d2941df28423124a48dd587bde1663671
nwiuser <nouisserzakariae@gmail.com>
2024-12-10 01:25:22 +0100

    Initial commit

--- build/ros_rl/prefix_override/sitecustomize.py
+++ build/ros_rl/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/zakaria/ros_ws/install/ros_rl'
+    sys.prefix = sys.exec_prefix = '/home/zakaria/ros2_rl/install/ros_rl'
