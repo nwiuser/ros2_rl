cc55d059eb071045da112e3277c5cceca00f414d
nwiuser <nouisserzakariae@gmail.com>
2024-12-11 10:08:11 +0100

    Commit2

--- src/ros_rl/setup.py
+++ src/ros_rl/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'ros_rl'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'model'), glob('model/*/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +24,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'test_node = ros_rl.test_node:main',
         ],
     },
 )
