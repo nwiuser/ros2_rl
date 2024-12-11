6fab4e09679fe5d0f60f305e727fd7af72abce58
nwiuser <nouisserzakariae@gmail.com>
2024-12-11 17:05:55 +0100

    Commit5

--- src/ros_rl/launch/launch.py
+++ src/ros_rl/launch/launch.py
@@ -1,8 +1,19 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
 
 def generate_launch_description():
     return LaunchDescription([
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+        ),
+
+        ExecuteProcess(
+            cmd=['gz', 'launch', 'gazebo.world'],
+            output='screen'
+        ),
         
         Node(
             package='ros_rl',
